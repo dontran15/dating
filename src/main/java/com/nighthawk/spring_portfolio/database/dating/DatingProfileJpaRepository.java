9095a85c0d11b6dade87ff1a384cb4295d10519b
horizon-ab <baileyasay@gmail.com>
2023-05-17 11:26:45 -0700

    jpa repository

---
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/DatingProfileJpaRepository.java
@@ -0,0 +1,17 @@
+package com.nighthawk.spring_portfolio.database.dating;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface DatingProfileJpaRepository extends JpaRepository<DatingProfile, Long> {
+
+    Optional<DatingProfile> findByEmail(String email);
+    Optional<DatingProfile> findByName(String name);
+
+    @Query(value = "SELECT * FROM DatingProfile d WHERE d.name LIKE ?1 or d.email LIKE ?1", nativeQuery = true)
+    List<DatingProfile> findByLikeTermNative(String term);
+    
+}
