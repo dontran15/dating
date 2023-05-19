9616e7e75b238edd82a6599afa973c7bc2d5e3ee
Don Tran <donqt15@gmail.com>
2023-05-19 10:48:21 -0700

    profile type superclass inheritance w springboot

--- src/main/java/com/nighthawk/spring_portfolio/database/dating/ProfileTypes/ProfileType.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/ProfileTypes/ProfileType.java
@@ -1,14 +1,26 @@
 package com.nighthawk.spring_portfolio.database.dating.ProfileTypes;
 
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class ProfileType {
-    public enum 
+    protected enum Type {
+        PERSONALITY, LIFESTYLE, INTEREST, MISC
+    }
+
     private String detail;
 
-    protected String getDetail() {
+    public String getDetail() {
         return this.detail;
     }
 
-    protected void setDetail(String detail) {
+    public void setDetail(String detail) {
         this.detail = detail;
+    }
+
+    public abstract String jsonString();
+
+    public ProfileType() {
+        this.detail = null;
     }
 }
