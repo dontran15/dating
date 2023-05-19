7dee82ea96b49db525c9b051b9091cc0bf0e133e
Don Tran <donqt15@gmail.com>
2023-05-19 12:15:20 -0700

    dating profile stuff

--- src/main/java/com/nighthawk/spring_portfolio/controllers/DatingProfileApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/DatingProfileApiController.java
@@ -1,52 +1,55 @@
-package com.nighthawk.spring_portfolio.controllers;
+// package com.nighthawk.spring_portfolio.controllers;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.nighthawk.spring_portfolio.database.dating.DatingProfile;
-import com.nighthawk.spring_portfolio.database.dating.DatingProfileJpaRepository;
+// import com.nighthawk.spring_portfolio.database.dating.DatingProfile;
+// import
+// com.nighthawk.spring_portfolio.database.dating.DatingProfileJpaRepository;
 
-@RestController
-@RequestMapping("/api/dating")
-public class DatingProfileApiController {
-    
-    @Autowired
-    DatingProfileJpaRepository datingProfileJpaRepository;
+// @RestController
+// @RequestMapping("/api/dating")
+// public class DatingProfileApiController {
 
-    @GetMapping("/all")
-    public ResponseEntity<Object> getDatingProfiles() {
-        Optional<List<DatingProfile>> profiles = datingProfileJpaRepository.findAllByOrderByNameAsc();
+// @Autowired
+// DatingProfileJpaRepository datingProfileJpaRepository;
 
-        if (profiles.get().isEmpty()) {
-            return new ResponseEntity<>("No dating profiles", HttpStatus.BAD_REQUEST);
-        }
+// @GetMapping("/all")
+// public ResponseEntity<Object> getDatingProfiles() {
+// Optional<List<DatingProfile>> profiles =
+// datingProfileJpaRepository.findAllByOrderByNameAsc();
 
-        return new ResponseEntity<Object>(profiles.get(), HttpStatus.OK);
-    }
+// if (profiles.get().isEmpty()) {
+// return new ResponseEntity<>("No dating profiles", HttpStatus.BAD_REQUEST);
+// }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getDatingProfile(@PathVariable long id) {
-        Optional<DatingProfile> profile = datingProfileJpaRepository.findById(id);
+// return new ResponseEntity<Object>(profiles.get(), HttpStatus.OK);
+// }
 
-        if (profile.get() == null) {
-            return new ResponseEntity<>("Dating profile not found", HttpStatus.BAD_REQUEST);
-        }
+// @GetMapping("/{id}")
+// public ResponseEntity<Object> getDatingProfile(@PathVariable long id) {
+// Optional<DatingProfile> profile = datingProfileJpaRepository.findById(id);
 
-        return new ResponseEntity<>(profile.get(), HttpStatus.OK);
-    }
+// if (profile.get() == null) {
+// return new ResponseEntity<>("Dating profile not found",
+// HttpStatus.BAD_REQUEST);
+// }
 
-    // I'll do this crap later
-    @GetMapping("/search/{term}")
-    public ResponseEntity<Object> getDatingProfilesByTerm() {
-        return new ResponseEntity<Object>(null, null, 0);
-    }
+// return new ResponseEntity<>(profile.get(), HttpStatus.OK);
+// }
 
-}
+// // I'll do this crap later
+// @GetMapping("/search/{term}")
+// public ResponseEntity<Object> getDatingProfilesByTerm() {
+// return new ResponseEntity<Object>(null, null, 0);
+// }
+
+// }
--- src/main/java/com/nighthawk/spring_portfolio/database/dating/DatingProfileJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/DatingProfileJpaRepository.java
@@ -1,26 +1,33 @@
-package com.nighthawk.spring_portfolio.database.dating;
+// package com.nighthawk.spring_portfolio.database.dating;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
 
-public interface DatingProfileJpaRepository extends JpaRepository<DatingProfile, Long> {
+// public interface DatingProfileJpaRepository extends
+// JpaRepository<DatingProfile, Long> {
 
-    Optional<DatingProfile> findByEmail(String email);
-    Optional<DatingProfile> findByName(String name);
-    Optional<DatingProfile> findById(long id);
-    Optional<List<DatingProfile>> findAllByOrderByNameAsc();
+// Optional<DatingProfile> findByEmail(String email);
 
-    @Query(value = "SELECT * FROM DatingProfile d WHERE d.name LIKE ?1 or d.email LIKE ?1", nativeQuery = true)
-    List<DatingProfile> findByLikeTermNative(String term);
+// Optional<DatingProfile> findByName(String name);
 
-    @Query(value = "SELECT * FROM DatingProfile d WHERE d.interests LIKE ?1", nativeQuery = true)
-    List<DatingProfile> findByLikeInterestsNative(ArrayList<String> interests);
+// Optional<DatingProfile> findById(long id);
 
-    // Fix this stuff
-    @Query(value = "SELECT * FROM DatingProfile d WHERE SELECT * FROM d.interests ", nativeQuery = true)
-    List<DatingProfile> findByLikeInterest(String interest);
-}
+// Optional<List<DatingProfile>> findAllByOrderByNameAsc();
+
+// @Query(value = "SELECT * FROM DatingProfile d WHERE d.name LIKE ?1 or d.email
+// LIKE ?1", nativeQuery = true)
+// List<DatingProfile> findByLikeTermNative(String term);
+
+// @Query(value = "SELECT * FROM DatingProfile d WHERE d.interests LIKE ?1",
+// nativeQuery = true)
+// List<DatingProfile> findByLikeInterestsNative(ArrayList<String> interests);
+
+// // Fix this stuff
+// @Query(value = "SELECT * FROM DatingProfile d WHERE SELECT * FROM d.interests
+// ", nativeQuery = true)
+// List<DatingProfile> findByLikeInterest(String interest);
+// }
