d76142f36d3a452a92ab940f604bf115ae946952
Don Tran <donqt15@gmail.com>
2023-05-19 10:08:05 -0700

    dating profile

--- src/main/java/com/nighthawk/spring_portfolio/database/dating/DatingProfile.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/DatingProfile.java
@@ -1,11 +1,15 @@
 package com.nighthawk.spring_portfolio.database.dating;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.nighthawk.spring_portfolio.database.person.Person;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,14 +20,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 public class DatingProfile {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;
+    private Person person;
 
-    private String email;
+    // personal profile
+    private Map<String, String> profile = new HashMap<>();
 
-    private ArrayList<String> interests;
+    // preference for profile ** (NOTE: not sure if we need this)
+    private Map<String, String> interests = new HashMap<>();
+
+    private static String[] types = { "personality1", "personality2", "personality3", "lifestyle1", "lifestyle2",
+            "lifestyle3", "song", "food", "quote", "movie", "ethnicity", "school", "job", "interest1", "interest2",
+            "interest3" };
+
+    public DatingProfile(Person person) {
+        this.person = person;
+        initializeProfile();
+    }
+
+    private void initializeProfile() {
+
+    }
 }
