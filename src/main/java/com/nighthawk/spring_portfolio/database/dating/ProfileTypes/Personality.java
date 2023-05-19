9480b5a9e7a7c5d7fc6a8892aaaf92b4fc7118ba
Don Tran <donqt15@gmail.com>
2023-05-19 10:08:26 -0700

    profile types

---
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/ProfileTypes/Personality.java
@@ -0,0 +1,22 @@
+package com.nighthawk.spring_portfolio.database.dating.ProfileTypes;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.nighthawk.spring_portfolio.database.person.Person;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Personality extends ProfileType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/ProfileTypes/ProfileType.java
@@ -0,0 +1,14 @@
+package com.nighthawk.spring_portfolio.database.dating.ProfileTypes;
+
+public abstract class ProfileType {
+    public enum 
+    private String detail;
+
+    protected String getDetail() {
+        return this.detail;
+    }
+
+    protected void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
