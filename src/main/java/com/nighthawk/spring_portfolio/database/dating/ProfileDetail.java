2f3778731c05c6ab6d3857b9db69d9c93b1d287a
Don Tran <donqt15@gmail.com>
2023-05-19 12:15:33 -0700

    profile detail stuff

---
+++ src/main/java/com/nighthawk/spring_portfolio/database/dating/ProfileDetail.java
@@ -0,0 +1,51 @@
+package com.nighthawk.spring_portfolio.database.dating;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.nighthawk.spring_portfolio.database.person.Person;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy.SelfInjection.Eager;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ProfileDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JoinColumn(name = "person_id")
+    @OneToOne(cascade = CascadeType.ALL)
+    private Person person;
+
+    // profile (NOTE: still deciding whether to consider sentiment analysis or
+    // making own algorithm for "pairings", probably don't need either)
+    @JoinColumn(name = "profileDetail_id")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private ArrayList<ProfileDetail> profileDetail = new ArrayList<>();
+
+    public ProfileDetail(Person person) {
+        this.person = person;
+        initializeProfile();
+    }
+
+    private void initializeProfile() {
+    }
+}
