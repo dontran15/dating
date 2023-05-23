3f21e405dd0b42a3eb94195384ea6a50a7dddd2b
horizon-ab <baileyasay@gmail.com>
2023-05-23 10:56:41 -0700

    chatgpt works now

--- src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
@@ -1,24 +1,26 @@
-// package com.nighthawk.spring_portfolio.database.chat;
+package com.nighthawk.spring_portfolio.database.chat;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-// import java.util.ArrayList;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("api/chat")
-// public class ChatGPTApiController {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
-// @GetMapping("/generate")
-// public ResponseEntity<Object> generatePickupLines(@RequestParam String
-// prompt, @RequestParam int responses) {
+@RestController
+@RequestMapping("api/chat")
+public class ChatGPTApiController {
 
-// ArrayList<String> response = (ArrayList<String>)
-// ChatGPT.generateMultiple(prompt, responses);
-// return new ResponseEntity<>(response, HttpStatus.OK);
-// }
+@GetMapping("/generate")
+public ResponseEntity<Object> generatePickupLines(@RequestParam String
+prompt, @RequestParam int responses) throws MalformedURLException, IOException {
 
-// }
+    ArrayList<String> response = (ArrayList<String>) ChatGPT.generateMultiple(prompt, responses);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+}
+
+}
