bc1edf696a934df8f21087317d467634d7127884
Don Tran <donqt15@gmail.com>
2023-05-22 21:11:39 -0700

    fixed maven stuff?

--- src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
@@ -1,22 +1,24 @@
-package com.nighthawk.spring_portfolio.database.chat;
+// package com.nighthawk.spring_portfolio.database.chat;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+// import java.util.ArrayList;
 
-@RestController
-@RequestMapping("api/chat")
-public class ChatGPTApiController {
+// @RestController
+// @RequestMapping("api/chat")
+// public class ChatGPTApiController {
 
-    @GetMapping("/generate")
-    public ResponseEntity<Object> generatePickupLines(@RequestParam String prompt, @RequestParam int responses) {
+// @GetMapping("/generate")
+// public ResponseEntity<Object> generatePickupLines(@RequestParam String
+// prompt, @RequestParam int responses) {
 
-        ArrayList<String> response = (ArrayList<String>) ChatGPT.generateMultiple(prompt, responses);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-    
-}
+// ArrayList<String> response = (ArrayList<String>)
+// ChatGPT.generateMultiple(prompt, responses);
+// return new ResponseEntity<>(response, HttpStatus.OK);
+// }
+
+// }
