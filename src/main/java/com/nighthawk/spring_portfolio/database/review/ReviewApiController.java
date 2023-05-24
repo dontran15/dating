c9bd3f6dca3793e4c81ede9cf4f24ea122a587d8
Hetvi Trivedi <hetvit27@gmail.com>
2023-05-23 22:28:06 -0700

    review api controller working now

--- src/main/java/com/nighthawk/spring_portfolio/database/review/ReviewApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/review/ReviewApiController.java
@@ -2,30 +2,21 @@ package com.nighthawk.spring_portfolio.database.review;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
-@RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/review") // all requests in file begin with this URI
+@RestController
+@RequestMapping("/api/review")
 public class ReviewApiController {
 
-    // Autowired enables Control to connect URI request and POJO Object to easily
-    // for Database CRUD operations
     @Autowired
     private ReviewJpaRepository repository;
 
-    /*
-     * GET List of Jokes
-     * 
-     * @GetMapping annotation is used for mapping HTTP GET requests onto specific
-     * handler methods.
-     */
     @GetMapping("/")
     public ResponseEntity<List<Review>> getReviewername() {
-        // ResponseEntity returns List of Jokes provide by JPA findAll()
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
@@ -34,14 +25,10 @@ public class ReviewApiController {
             @RequestParam("Age") int age,
             @RequestParam("Review") String reviewtext,
             @RequestParam("Contact") String contact) {
-        repository.save(new Review(null, reviewername, age, reviewtext, contact)); // JPA save
-        long maxId = repository.getMaxId();
-        Optional<Review> optional = repository.findById(maxId);
-        if (optional.isPresent()) {
-            Review review1 = optional.get();
-            return new ResponseEntity(review1, HttpStatus.OK);
+        Review savedReview = repository.save(new Review(null, reviewername, age, reviewtext, contact));
+        if (savedReview != null) {
+            return new ResponseEntity<>(savedReview, HttpStatus.OK);
         }
-        // Bad ID
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Failed HTTP response: status code, headers, and body
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
