0239fae23ca5faf5d5022c37279ba4ff1bfcc6ba
horizon-ab <baileyasay@gmail.com>
2023-05-23 19:53:09 -0700

    try autowired

--- src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/chat/ChatGPTApiController.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.database.chat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 @RequestMapping("api/chat")
 public class ChatGPTApiController {
 
+    @Autowired
     private ChatGPT chatGPT = new ChatGPT();
 
     @GetMapping("/generate")
