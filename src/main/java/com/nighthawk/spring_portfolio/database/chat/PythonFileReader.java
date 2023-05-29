5434f35b73ad99db2b22eb8888a0816b58973bc4
Don Tran <donqt15@gmail.com>
2023-05-28 21:51:47 -0700

    python file reader

---
+++ src/main/java/com/nighthawk/spring_portfolio/database/chat/PythonFileReader.java
@@ -0,0 +1,42 @@
+package com.nighthawk.spring_portfolio.database.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class PythonFileReader {
+    public static void main(String[] args) {
+        try {
+            // Create the ProcessBuilder and specify the command
+            ProcessBuilder pb = new ProcessBuilder("python",
+                    "src/main/java/com/nighthawk/spring_portfolio/database/chat/Chat.py");
+
+            // Redirect the output of the Python process
+            pb.redirectErrorStream(true);
+
+            // Start the process
+            Process process = pb.start();
+
+            // Get the InputStream of the process's output
+            InputStream inputStream = process.getInputStream();
+
+            // Read the output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+
+            // Print the output
+            System.out.println("Exit Code: " + exitCode);
+            System.out.println("Output:\n" + output.toString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
